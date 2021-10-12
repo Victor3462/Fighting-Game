7a523d952c0f9850cf243b7841167a3a43c9b245
Victor Leiderö-Palmlöv <victorleideropalmlov@gmail.com>
2021-10-12 11:04:37 +0200

    Förättringar

--- Fighting-Game/Program.cs
+++ Fighting-Game/Program.cs
@@ -1,92 +1,102 @@
 ﻿using System;
 
-Console.WriteLine(" ");
-
-tb.displayfight();
-Console.WriteLine(" ");
-tb.WriteCentered("PRESS ENTER TO START");
-Console.ReadLine();
-Console.Clear();
-
-Console.WriteLine(" ");
-tb.displayfight();
-Console.WriteLine(" ");
-string p1name = tb.getplayer1name();
-Console.Clear();
-Console.WriteLine(" ");
-tb.displayfight();
-Console.WriteLine(" ");
-string p2name = tb.getplayer2name();
-Console.Clear();
-
-
-int player1hp = 50;
-int player2hp = 50;
-
-tb.displayfight();
-Console.WriteLine(" ");
-
-Random rnd = new Random();
-
-
-
-while (player1hp > 0 && player2hp > 0)
+string restart1 = "yes";
+while (restart1 == "yes")
 {
+    Console.Clear();
+    Console.WriteLine(" ");
 
-    int damage = rnd.Next(1, 20);
-
-    player1hp = player1hp - damage;
-    tb.WriteCentered($"{p1name} got hit by {p2name} and lost {damage} hp!");
+    tb.displayfight();
+    Console.WriteLine(" ");
+    tb.WriteCentered("PRESS ENTER TO START");
     Console.ReadLine();
+    Console.Clear();
 
-    damage = rnd.Next(1, 20);
-
-
-    player2hp = player2hp - damage;
-    tb.WriteCentered($"{p2name} got hit by {p1name} and lost {damage} hp!");
-    Console.ReadLine();
-
-}
-
-if(player1hp < 1)
-{
+    Console.WriteLine(" ");
+    tb.displayfight();
+    Console.WriteLine(" ");
+    string p1name = tb.getplayer1name();
     Console.Clear();
     Console.WriteLine(" ");
     tb.displayfight();
     Console.WriteLine(" ");
-    tb.WriteCentered($"{p2name} defeated the mighty {p1name} in a fight to the death!");
-    Console.ReadLine();
-    
+    string p2name = tb.getplayer2name();
+    Console.Clear();
+
+
+    int player1hp = 50;
+    int player2hp = 50;
+
+
+    Random rnd = new Random();
+
+
+
+    while (player1hp > 0 && player2hp > 0)
+    {
+
+        int damage = rnd.Next(1, 20);
+
+        tb.displayfight();
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        tb.fighters();
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+
+        player1hp = player1hp - damage;
+        tb.WriteCentered($"{p1name} got hit by {p2name} and lost {damage} hp! {player1hp} hp left");
+        Console.ReadLine();
+
+        damage = rnd.Next(1, 20);
+
+
+        player2hp = player2hp - damage;
+        tb.WriteCentered($"{p2name} got hit by {p1name} and lost {damage} hp!  {player2hp} hp left");
+        Console.ReadLine();
+        Console.Clear();
+
     }
 
-else if(player2hp < 1)
-{
-    Console.Clear();
-    Console.WriteLine(" ");
-    tb.displayfight();
-    Console.WriteLine(" ");
-    tb.WriteCentered($"{p1name} defeated the mighty {p2name} in a fight to the death!");
-    Console.ReadLine();
-    
-}    
-Console.Clear();
-Console.WriteLine(" ");
-tb.displayend();
-Console.WriteLine(" ");
-tb.WriteCentered("Thanks for playing!");
-Console.ReadLine();
+    if (player1hp < 1)
+    {
+        Console.Clear();
+        Console.WriteLine(" ");
+        tb.displayfight();
+        Console.WriteLine(" ");
+        tb.WriteCentered($"{p2name} defeated the mighty {p1name} in a fight to the death!");
+        Console.ReadLine();
 
+    }
 
+    else if (player2hp < 1)
+    {
+        Console.Clear();
+        Console.WriteLine(" ");
+        tb.displayfight();
+        Console.WriteLine(" ");
+        tb.WriteCentered($"{p1name} defeated the mighty {p2name} in a fight to the death!");
+        Console.ReadLine();
 
+    }
 
+    restart1 = "";
 
+    while (restart1 == "")
+    {
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        tb.WriteCentered("Do you want to play again?");
+        tb.WriteCentered("Yes or no?");
+        restart1 = Console.ReadLine().ToLower();
+    }
 
-
-
-
-
-
-
-
-
-
+    if (restart1 == "no")
+    {
+        tb.displayend();
+        Console.ReadLine();
+        Environment.Exit(0);
+    }
+}
--- Fighting-Game/toolbox.cs
+++ Fighting-Game/toolbox.cs
@@ -1,86 +1,94 @@
 using System;
 public class tb
- {
+{
 
-public static void WriteCentered(string text){
-
-    int a = Console.WindowWidth / 2;
-
-    a -= text.Length / 2;
-
-    for(int i = 0; i < a; i++)
+    public static void WriteCentered(string text)
     {
-        Console.Write(" ");
+
+        int a = Console.WindowWidth / 2;
+
+        a -= text.Length / 2;
+
+        for (int i = 0; i < a; i++)
+        {
+            Console.Write(" ");
+        }
+        Console.WriteLine(text);
     }
-    Console.WriteLine(text);
-}
-// Gör så att det jag skriver i consolen blir centrerat
+    // Gör så att det jag skriver i consolen blir centrerat
 
-     public static string getplayer1name()
-     {
-         WriteCentered("Write The name of player 1:");
+    public static string getplayer1name()
+    {
+        WriteCentered("Write The name of player 1:");
 
-         string playername1 = Console.ReadLine();
-         
-        while(playername1 == "") {
+        string playername1 = Console.ReadLine();
+
+        while (playername1 == "")
+        {
             WriteCentered("Write The name of player 1:");
             playername1 = Console.ReadLine();
         }
 
         return playername1;
-        }
-        // Gör en string för playername2 och frågar spelaren att skriva namn för den och vägrar fortsätta utan namnet.
+    }
+    // Gör en string för playername2 och frågar spelaren att skriva namn för den och vägrar fortsätta utan namnet.
 
     public static string getplayer2name()
     {
         WriteCentered("Write The name of player 2:");
 
         string playername2 = Console.ReadLine();
-        
-        while(playername2 == ""){
+
+        while (playername2 == "")
+        {
             WriteCentered("Write the name of player 2:");
             playername2 = Console.ReadLine();
-            
+
         }
 
-    return playername2;
+        return playername2;
 
     }
     // Gör en string för playername 2 och frågar spelaren att skriva namn för den och vägrar fortsätta utan namnet
 
-    public static void displayfight(){
-Console.ForegroundColor = ConsoleColor.Red;
-tb.WriteCentered(" #######                       ");
-tb.WriteCentered(" #       #  ####  #    # ##### ");
-tb.WriteCentered(" #       # #    # #    #   #   ");
-tb.WriteCentered(" #####   # #      ######   #   ");
-tb.WriteCentered(" #       # #  ### #    #   #   ");
-tb.WriteCentered(" #       # #    # #    #   #   ");
-tb.WriteCentered(" #       #  ####  #    #   #   ");
-Console.ForegroundColor = ConsoleColor.White;
-}
-// Gör så att jag slipper göra ascci art varje gång jag behöver den
-
-public static void displayend(){
-    Console.ForegroundColor = ConsoleColor.Red;
-    tb.WriteCentered("  ::::::::::: :::    ::: ::::::::::          :::::::::: ::::    ::: ::::::::: ");
-    tb.WriteCentered("     :+:     :+:    :+: :+:                 :+:        :+:+:   :+: :+:    :+: ");
-    tb.WriteCentered("    +:+     +:+    +:+ +:+                 +:+        :+:+:+  +:+ +:+    +:+  ");
-    tb.WriteCentered("   +#+     +#++:++#++ +#++:++#            +#++:++#   +#+ +:+ +#+ +#+    +:+   ");
-    tb.WriteCentered("  +#+     +#+    +#+ +#+                 +#+        +#+  +#+#+# +#+    +#+    ");
-    tb.WriteCentered(" #+#     #+#    #+# #+#                 #+#        #+#   #+#+# #+#    #+#     ");
-    tb.WriteCentered("###     ###    ### ##########          ########## ###    #### #########       ");
-    
+    public static void displayfight()
     {
-         
+        Console.ForegroundColor = ConsoleColor.Red;
+        tb.WriteCentered(" #######                       ");
+        tb.WriteCentered(" #       #  ####  #    # ##### ");
+        tb.WriteCentered(" #       # #    # #    #   #   ");
+        tb.WriteCentered(" #####   # #      ######   #   ");
+        tb.WriteCentered(" #       # #  ### #    #   #   ");
+        tb.WriteCentered(" #       # #    # #    #   #   ");
+        tb.WriteCentered(" #       #  ####  #    #   #   ");
+        Console.ForegroundColor = ConsoleColor.White;
     }
-}
+    // Gör så att jag slipper göra ascci art varje gång jag behöver den
+
+    public static void displayend()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        tb.WriteCentered("  ::::::::::: :::    ::: ::::::::::          :::::::::: ::::    ::: ::::::::: ");
+        tb.WriteCentered("     :+:     :+:    :+: :+:                 :+:        :+:+:   :+: :+:    :+: ");
+        tb.WriteCentered("    +:+     +:+    +:+ +:+                 +:+        :+:+:+  +:+ +:+    +:+  ");
+        tb.WriteCentered("   +#+     +#++:++#++ +#++:++#            +#++:++#   +#+ +:+ +#+ +#+    +:+   ");
+        tb.WriteCentered("  +#+     +#+    +#+ +#+                 +#+        +#+  +#+#+# +#+    +#+    ");
+        tb.WriteCentered(" #+#     #+#    #+# #+#                 #+#        #+#   #+#+# #+#    #+#     ");
+        tb.WriteCentered("###     ###    ### ##########          ########## ###    #### #########       ");
+
+        {
+
+        }
+    }
 
 
+    public static void fighters()
+    {
 
+        WriteCentered("(*-*)-|  |-(^-^)");
+        
+        }
 
-
-    
 
 
 }
